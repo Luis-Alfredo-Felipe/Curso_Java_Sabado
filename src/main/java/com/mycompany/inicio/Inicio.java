a218f58379ef21063e69cfc9a0a91d4943c4b973
Luis-Alfredo-Felipe <genttilez@gmail.com>
2022-08-20 16:26:17 -0600

    first commit

---
+++ src/main/java/com/mycompany/inicio/Inicio.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.inicio;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        System.out.println("Hola Luis Felipe");
+    }
+}
